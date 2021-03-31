d68823b1a028f2d2c5e1cb9376594e2fe86ddf5e
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-03-31 14:07:21 +0600

    add

---
+++ cl.py
@@ -0,0 +1,34 @@
+# importing whole module
+from tkinter import *
+from tkinter.ttk import *
+
+# importing strftime function to
+# retrieve system's time
+from time import strftime
+
+# creating tkinter window
+root = Tk()
+root.title('Clock by Rifat')
+
+# This function is used to
+# display time on the label
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
+
+# Styling the label widget so that clock
+# will look more attractive
+lbl = Label(root, font=('ds-digital', 80, 'bold'),
+            background='black',
+            foreground='cyan')
+
+# Placing clock at the centre
+# of the tkinter window
+lbl.pack(anchor='center')
+time()
+
+mainloop()
